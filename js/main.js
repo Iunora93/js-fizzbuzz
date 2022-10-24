fd3c4eccbea3d58cd014f9051aa3001743d152f7
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-24 15:16:30 +0200

    Condition

--- js/main.js
+++ js/main.js
@@ -9,6 +9,21 @@ BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append(
 BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
 
+
 for ( let i = 1; i <= 100; i++) {
-    console.log(i);
+    console.log(i); 
+    if(i === 3){
+    console.log(`Fizz`);
+}     
 }
+
+
+  
+
+
+
+
+
+
+
+
